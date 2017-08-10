10e585088f6e897ebd6b99b580ff84c41571c686
MarcelGar6 <marcel.m.garcez@gmail.com>
2017-08-09 23:32:15 -0300

    Crud web

---
+++ CRUD/Controllers/HomeController.cs
@@ -0,0 +1,105 @@
+﻿using CRUD.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CRUD.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetAlunos()
+        {
+            using (CrudDatabaseEntities dc = new CrudDatabaseEntities())
+            {
+                var alunos = dc.Alunos.OrderBy(a => a.Nome).ToList();
+                return Json(new { data = alunos }, JsonRequestBehavior.AllowGet);
+
+            }
+        }
+
+        [HttpGet]
+        public ActionResult Save(int id)
+        {
+            using (CrudDatabaseEntities dc = new CrudDatabaseEntities())
+            {
+                var v = dc.Alunos.Where(a => a.IdAluno == id).FirstOrDefault();
+                return View(v);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Save(Aluno alu)
+        {
+            bool status = false;
+            if (ModelState.IsValid)
+            {
+                using (CrudDatabaseEntities dc = new CrudDatabaseEntities())
+                {
+                    if(alu.IdAluno > 0)
+                    {
+                        //Edit
+                        var v = dc.Alunos.Where(a => a.IdAluno == alu.IdAluno).FirstOrDefault();
+                        if(v != null)
+                        {
+                            v.Nome = alu.Nome;
+                            v.Sobrenome = alu.Sobrenome;
+                            v.Email = alu.Email;
+                            v.Cidade = alu.Cidade;
+                            v.Pais = alu.Pais;
+                        }
+                    }
+                    else
+                    {
+                        //Save
+                        dc.Alunos.Add(alu);
+                    }
+                    dc.SaveChanges();
+                    status = true;
+                }
+            }
+            return new JsonResult { Data = new { status = status } };
+        }
+
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
+            using (CrudDatabaseEntities dc = new CrudDatabaseEntities())
+            {
+                var v = dc.Alunos.Where(a => a.IdAluno == id).FirstOrDefault();
+                if(v != null)
+                {
+                    return View(v);
+                }
+                else
+                {
+                    return HttpNotFound();
+                }
+            }
+        }
+
+        [HttpPost]
+        [ActionName("Delete")]
+        public ActionResult DeleteAluno(int id)
+        {
+            bool status = false;
+            using (CrudDatabaseEntities dc = new CrudDatabaseEntities())
+            {
+                var v = dc.Alunos.Where(a => a.IdAluno == id).FirstOrDefault();
+                if(v != null)
+                {
+                    dc.Alunos.Remove(v);
+                    dc.SaveChanges();
+                    status = true;
+                }
+            }
+            return new JsonResult { Data = new { status = status } };
+        }
+    }
+}
---
+++ CRUD/Models/Extended/Aluno.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CRUD.Models
+{
+    [MetadataType(typeof(AlunoMetadata))]
+    public partial class Aluno
+    {
+
+    }
+    public class AlunoMetadata
+    {
+        [Required (AllowEmptyStrings = false, ErrorMessage = "Preencha seu nome")]
+        public string Nome { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Preencha seu sobrenome")]
+        public string Sobrenome { get; set; }
+    }
+}
